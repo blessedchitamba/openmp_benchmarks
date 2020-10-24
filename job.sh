03951a5db13f52221faee80e9f67aa0d20915361
Blessed-Brighton <40063222+blessedchitamba@users.noreply.github.com>
2020-10-24 17:16:47 +0200

    Update job.sh

--- job.sh
+++ job.sh
@@ -6,11 +6,11 @@
 #SBATCH --mem-per-cpu=2000
 
 #SBATCH --job-name="tim_hpc_a1_1"
-#SBATCH --mail-user=smntim001@myuct.ac.za
+#SBATCH --mail-user=blessedbrighton@live.com
 #SBATCH --mail-type=ALL
 
 export OMP_NUM_THREADS=32
-./arraybench_59049 > arraybench_59049.out
-#./schedbench --ntasks=1 > schedbench.out
-#./syncbench --ntasks=1 > syncbench.out
-#./taskbench --ntasks=1 > taskbench.out
+#./arraybench_59049 > arraybench_59049.out
+./schedbench --ntasks=1 > schedbench.txt
+./syncbench --ntasks=1 > syncbench.txt
+./taskbench --ntasks=1 > taskbench.txt
