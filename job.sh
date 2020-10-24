74b06016eb584d3044bb2d945850551151435292
Blessed-Brighton <40063222+blessedchitamba@users.noreply.github.com>
2020-10-24 17:14:02 +0200

    Update job.sh

--- job.sh
+++ job.sh
@@ -1,20 +1,16 @@
 #!/bin/sh
-
 #SBATCH --account=icts
-
 #SBATCH --partition=curie
-
-#SBATCH --nodes=1 --ntasks=16
-
+#SBATCH --nodes=1 --ntasks 1 --cpus-per-task 32
 #SBATCH --time=10:00
+#SBATCH --mem-per-cpu=2000
 
-#SBATCH --mem-per-cpu=4000
-
-#SBATCH --job-name="Claude1"
-
-#SBATCH --mail-user=formanekjc@gmail.com
-
+#SBATCH --job-name="tim_hpc_a1_1"
+#SBATCH --mail-user=smntim001@myuct.ac.za
 #SBATCH --mail-type=ALL
 
 export OMP_NUM_THREADS=32
-./openmpbench_C_v31/schedbench --outer-repetitions 100 > shedbench.txt
+./arraybench_59049 > arraybench_59049.out
+#./schedbench --ntasks=1 > schedbench.out
+#./syncbench --ntasks=1 > syncbench.out
+#./taskbench --ntasks=1 > taskbench.out
